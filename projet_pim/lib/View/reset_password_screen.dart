4f5377ce4d2db393b3236de9bc91b49a71d1ae77
nadabha1 <100917479+nadabha1@users.noreply.github.com>
2025-02-12 20:18:38 +0100

    n

--- projet_pim/lib/View/reset_password_screen.dart
+++ projet_pim/lib/View/reset_password_screen.dart
@@ -1,102 +1,18 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:projet_pim/Providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
-class ResetPasswordScreen extends StatefulWidget {
+class ResetPasswordScreen extends StatelessWidget {
   final String email;
+  final TextEditingController _otpController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   ResetPasswordScreen({required this.email});
 
   @override
-  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
-}
-
-class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
-  final TextEditingController _otpController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _isOtpVerified = false;
-  bool _isLoading = false;
-
-  // Base URL for the API
-  final String baseUrl = "http://10.0.2.2:3000/auth"; // Replace with your API base URL
-
-  // Function to verify OTP
-  Future<void> _verifyOtp() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/verify-otp'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({
-          'email': widget.email,
-          'otp': _otpController.text,
-        }),
-      );
-
-      if (response.statusCode == 201||response.statusCode == 200) {
-        setState(() {
-          _isOtpVerified = true;
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('OTP verified successfully')),
-        );
-      } else {
-        final error = jsonDecode(response.body)['error'] ?? 'Error verifying OTP';
-        throw Exception(error);
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error verifying OTP: ${e.toString()}')),
-      );
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  // Function to reset password
-  Future<void> _resetPassword() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/reset-password-with-otp'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({
-          'email': widget.email,
-          'otp': _otpController.text,
-          'password': _passwordController.text,
-        }),
-      );
-
-      if (response.statusCode == 201||response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Password reset successful')),
-        );
-        Navigator.pushReplacementNamed(context, '/login');
-      } else {
-        final error = jsonDecode(response.body)['error'] ?? 'Error resetting password';
-        throw Exception(error);
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error resetting password: ${e.toString()}')),
-      );
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final resetPasswordProvider = Provider.of<AuthProvider>(context);
+
     return Scaffold(
       backgroundColor: const Color(0xFFF7F4FC),
       body: Padding(
@@ -108,7 +24,7 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
               children: [
                 // Title
                 Text(
-                  _isOtpVerified ? "Set New Password" : "Verify OTP",
+                  resetPasswordProvider.isOtpVerified ? "Set New Password" : "Verify OTP",
                   style: const TextStyle(
                     fontSize: 34,
                     fontWeight: FontWeight.bold,
@@ -117,9 +33,10 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
                 ),
                 const SizedBox(height: 10),
 
+
                 // Subtitle
                 Text(
-                  _isOtpVerified
+                  resetPasswordProvider.isOtpVerified
                       ? "Enter your new password below"
                       : "Enter the OTP sent to your email",
                   style: TextStyle(
@@ -130,77 +47,73 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
                 const SizedBox(height: 40),
 
                 // OTP Input
-                if (!_isOtpVerified)
+                if (!resetPasswordProvider.isOtpVerified)
                   TextField(
                     controller: _otpController,
                     decoration: InputDecoration(
                       labelText: 'OTP',
-                      labelStyle: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 14,
-                      ),
+                      labelStyle: TextStyle(color: Colors.grey[700], fontSize: 14),
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12),
                         borderSide: BorderSide.none,
                       ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 16,
-                        horizontal: 16,
-                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
                     ),
                     keyboardType: TextInputType.number,
                   ),
-                if (!_isOtpVerified) const SizedBox(height: 20),
+                if (!resetPasswordProvider.isOtpVerified) const SizedBox(height: 20),
 
                 // New Password Input
-                if (_isOtpVerified)
+                if (resetPasswordProvider.isOtpVerified)
                   TextField(
                     controller: _passwordController,
                     decoration: InputDecoration(
                       labelText: 'New Password',
-                      labelStyle: TextStyle(
-                        color: Colors.grey[700],
-                        fontSize: 14,
-                      ),
+                      labelStyle: TextStyle(color: Colors.grey[700], fontSize: 14),
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12),
                         borderSide: BorderSide.none,
                       ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 16,
-                        horizontal: 16,
-                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
                     ),
                     obscureText: true,
                   ),
-                if (_isOtpVerified) const SizedBox(height: 20),
+                if (resetPasswordProvider.isOtpVerified) const SizedBox(height: 20),
 
                 // Verify or Reset Button
-                _isLoading
+                resetPasswordProvider.isLoading
                     ? const Center(child: CircularProgressIndicator())
                     : ElevatedButton(
-                        onPressed: _isOtpVerified ? _resetPassword : _verifyOtp,
+                        onPressed: () {
+                          if (resetPasswordProvider.isOtpVerified) {
+                            resetPasswordProvider.resetPassword(
+                              context,
+                              email,
+                              _otpController.text,
+                              _passwordController.text,
+                            );
+                          } else {
+                            resetPasswordProvider.verifyOtp(
+                              context,
+                              email,
+                              _otpController.text,
+                            );
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: const Color(0xFF6A1B9A), // Purple button
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(12),
                           ),
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 16,
-                          ),
-                          textStyle: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         child: Center(
-                          child: Text(
-                            _isOtpVerified ? 'Reset Password' : 'Verify OTP',
-                          ),
+                          child: Text(resetPasswordProvider.isOtpVerified ? 'Reset Password' : 'Verify OTP'),
                         ),
                       ),
               ],
